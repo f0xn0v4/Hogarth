9d147bf1e2d03851b12f0d6d979598005a509f22
Shashwath Murthy <shashwath.murthy@gmail.com>
2025-08-13 19:13:36 -0500

    feat: added redirect from category archive to user-created pages

--- functions.php
+++ functions.php
@@ -16,8 +16,69 @@ function prevent_future_type( $post_data ) {
 }
 
 add_action('wp_enqueue_scripts', 'hogarth_enqueue_styles');
+add_action('template_redirect', function () {
+    // Check if the current page is the "blog" category archive
+    if (is_category('blog')) {
+        // Query for a page with the title "Blog"
+        $query = new WP_Query([
+            'post_type'      => 'page',
+            'title'          => 'Blog',
+            'posts_per_page' => 1
+        ]);
 
+        if ($query->have_posts()) {
+            $query->the_post();
+            $url = get_permalink();
+            wp_reset_postdata();
 
+            // Redirect to the found page
+            wp_redirect($url, 301);
+            exit;
+        }
+    }
+});
+add_action('template_redirect', function () {
+    // Check if the current page is the "blog" category archive
+    if (is_category('event')) {
+        // Query for a page with the title "Blog"
+        $query = new WP_Query([
+            'post_type'      => 'page',
+            'title'          => 'Events',
+            'posts_per_page' => 1
+        ]);
+
+        if ($query->have_posts()) {
+            $query->the_post();
+            $url = get_permalink();
+            wp_reset_postdata();
+
+            // Redirect to the found page
+            wp_redirect($url, 301);
+            exit;
+        }
+    }
+});
+add_action('template_redirect', function () {
+    // Check if the current page is the "blog" category archive
+    if (is_category('artwork')) {
+        // Query for a page with the title "Blog"
+        $query = new WP_Query([
+            'post_type'      => 'page',
+            'title'          => 'Gallery',
+            'posts_per_page' => 1
+        ]);
+
+        if ($query->have_posts()) {
+            $query->the_post();
+            $url = get_permalink();
+            wp_reset_postdata();
+
+            // Redirect to the found page
+            wp_redirect($url, 301);
+            exit;
+        }
+    }
+});
 add_filter('wp_insert_post_data', 'prevent_future_type');
 remove_action('future_post', '_future_post_hook');
 
